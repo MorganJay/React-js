ea8bdaec70d78c9c4c6a5162447328f01e0a98d8
James Morgan <MorganJay@users.noreply.github.com>
2021-04-23 17:27:50 +0100

    validated form using joi

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -6,28 +6,32 @@ import Input from './common/input';
 class LoginForm extends Component {
   state = {
     account: { username: '', password: '' },
-    errors: {  }
+    errors: {}
   };
 
   schema = {
-    username: Joi.string().required(),
-    password: Joi.string().required()
+    username: Joi.string().required().label('Username'),
+    password: Joi.string().required().label('Password')
   };
 
   validate = () => {
-    const result = Joi.validate(this.state.account, this.schema, {
-      abortEarly: false
-    });
-    console.log(result);
+    const options = { abortEarly: false };
+    const {error} = Joi.validate(this.state.account, this.schema, options);
+    if (!error) return null;
 
     const errors = {};
-    const {
-      account: { username, password }
-    } = this.state;
-    if (username.trim() === '') errors.username = 'Username is required.';
-    if (password.trim() === '') errors.password = 'Password is required.';
+    for (let item of error.details) errors[item.path[0]] = item.message;
 
-    return Object.keys(errors).length === 0 ? null : errors; // ? why doesn't the form submit when I return {} instead of null
+    return errors;
+
+    // const errors = {};
+    // const {
+    //   account: { username, password }
+    // } = this.state;
+    // if (username.trim() === '') errors.username = 'Username is required.';
+    // if (password.trim() === '') errors.password = 'Password is required.';
+
+    // return Object.keys(errors).length === 0 ? null : errors; // ? why doesn't the form submit when I return {} instead of null
   };
 
   handleSubmit = e => {
@@ -41,23 +45,23 @@ class LoginForm extends Component {
     console.log('Submitted');
   };
 
-  validateProperty = ({ name, value }) => {
-    if (name === 'username') {
-      if (value.trim() === '') return 'Username is required.';
-      // ...
-    }
-    if (name === 'password') {
-      if (value.trim() === '') return 'Password is required.';
-      // ...
-    }
-  };
+  // validateProperty = ({ name, value }) => {
+  //   if (name === 'username') {
+  //     if (value.trim() === '') return 'Username is required.';
+  //     // ...
+  //   }
+  //   if (name === 'password') {
+  //     if (value.trim() === '') return 'Password is required.';
+  //     // ...
+  //   }
+  // };
 
   handleChange = ({ currentTarget: input }) => {
     const { name, value } = input;
     const errors = { ...this.state.errors };
-    const errorMessage = this.validateProperty(input);
-    if (errorMessage) errors[name] = errorMessage;
-    else delete errors[name];
+    // const errorMessage = this.validateProperty(input);
+    // if (errorMessage) errors[name] = errorMessage;
+    // else delete errors[name];
 
     const account = { ...this.state.account };
     account[name] = value;
