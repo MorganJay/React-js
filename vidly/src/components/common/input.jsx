c67123d2c8cac1cbf9cbf6e5aa06a809cf926a3b
James Morgan <MorganJay@users.noreply.github.com>
2021-04-23 17:01:48 +0100

    Added validation on change

--- vidly/src/components/common/input.jsx
+++ vidly/src/components/common/input.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 
-const Input = ({ name, label, value, type, onChange }) => {
+const Input = ({ name, label, value, type, onChange, error, ...props }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
       <input
+        {...props}
         autoFocus
         type={type}
-        className="form-control"
+        className="form-control mb-2"
         id={name}
         name={name}
         value={value}
         onChange={onChange}
       />
+      {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
 };
--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -7,23 +7,55 @@ class LoginForm extends Component {
     errors: { username: '', password: '' }
   };
 
-  handleSubmit = e => {
-    e.preventDefault();
-
-    // Call the server
-    console.log(`Submitted - username ${username}`);
-  };
-
-  handleChange = ({ currentTarget: input }) => {
-    const account = { ...this.state.account };
-    account[input.name] = input.value;
-    this.setState({ account });
-  };
-
-  render() {
+  validate = () => {
+    const errors = {};
     const {
       account: { username, password }
     } = this.state;
+    if (username.trim() === '') errors.username = 'Username is required.';
+    if (password.trim() === '') errors.password = 'Password is required.';
+
+    return Object.keys(errors).length === 0 ? null : errors; // ? why doesn't the form submit when I return {} instead of null
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const errors = this.validate();
+
+    this.setState({ errors: errors || {} }); // not clean
+    if (errors) return;
+
+    // Call the server
+    console.log('Submitted');
+  };
+
+  validateProperty = ({ name, value }) => {
+    if(name === 'username'){
+      if (value.trim() === '') return 'Username is required.';
+      // ...
+    }
+    if(name === 'password'){
+      if (value.trim() === '') return 'Password is required.';
+      // ...
+    }
+
+  }
+
+  handleChange = ({ currentTarget: input }) => {
+    const { name, value } = input;
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[name] = errorMessage;
+    else delete errors[name];
+
+    const account = { ...this.state.account };
+    account[name] = value;
+
+    this.setState({ account, errors });
+  };
+
+  render() {
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -32,16 +64,18 @@ class LoginForm extends Component {
             label="Username"
             name="username"
             type="text"
-            value={username}
+            value={account.username}
             onChange={this.handleChange}
+            error={errors.username}
           />
 
           <Input
             label="Password"
             name="password"
             type="password"
-            value={password}
+            value={account.password}
             onChange={this.handleChange}
+            error={errors.password}
           />
 
           <button className="btn btn-primary">Login</button>
