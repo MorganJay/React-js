7f24e26aac664d34f9823a9568aab39a35700495
James Morgan <MorganJay@users.noreply.github.com>
2021-04-24 12:29:49 +0100

    disabled button feature

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -90,7 +90,9 @@ class LoginForm extends Component {
             error={errors.password}
           />
 
-          <button className="btn btn-primary">Login</button>
+          <button disabled={this.validate()} className="btn btn-primary">
+            Login
+          </button>
         </form>
       </div>
     );
