a600f3fb74208fb93325e351f480c0ae0c9308e4
James Morgan <MorganJay@users.noreply.github.com>
2021-04-25 23:21:15 +0100

    disabled authentication

--- vidly-api-node/index.js
+++ vidly-api-node/index.js
@@ -1,16 +1,16 @@
-const winston = require("winston");
-const express = require("express");
-const config = require("config");
+const winston = require('winston');
+const express = require('express');
+const config = require('config');
 const app = express();
 
-require("./startup/logging")();
-require("./startup/cors")(app);
-require("./startup/routes")(app);
-require("./startup/db")();
-require("./startup/config")();
-require("./startup/validation")();
+// require("./startup/logging")();
+require('./startup/cors')(app);
+require('./startup/routes')(app);
+require('./startup/db')();
+require('./startup/config')();
+require('./startup/validation')();
 
-const port = process.env.PORT || config.get("port");
+const port = process.env.PORT || config.get('port');
 const server = app.listen(port, () =>
   winston.info(`Listening on port ${port}...`)
 );
